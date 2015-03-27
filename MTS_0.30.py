13f03941ece0a70e738e0751eccd90f17a8ec3af
Andrew Williams <williamsr.andrew@gmail.com>
2015-03-26 22:30:35 -0500

    Updated to create file
    Now creates file and polls TIVA for all the data it will be receiving in
    the future

--- MTS_0.30.py
+++ MTS_0.30.py
@@ -10,8 +10,8 @@ from tkinter.ttk import Frame, Button, Style
 
 ####### Init variables #######
 ser = serial.Serial(baudrate = 115200, timeout = 0.5)
-hs_bit = b'p'		# Handshake bit
-termination_bit = b'!'
+hs_byte = b'p'		# Handshake byte
+termination_byte = b'~'
 inits = 0
 
 
@@ -22,7 +22,7 @@ class Motor(Frame):
 		self.parent = parent
 		self.initUI()
 		# self.initPort()
-		# self.handshake(hs_bit)
+		# self.handshake(hs_byte)
 
 	def initUI(self):
 		self.parent.title("Motor Test")
@@ -99,19 +99,20 @@ class Motor(Frame):
 			self.updateView()
 
 	def runTest4(self):
-		# Timer
-		curTime = time.time()
 
 		if ser.name == None:
 			lb.insert(END, 'Open comunication on a port to run a test')
 			self.updateView()
 			return
 		try:
-			ser.write('4'.encode('utf-8'))
 			lb.insert(END, 'Running test 4...')
 			self.updateView()
-			bits_read = self.getData()
-			print("Time: {}\nBits read: {}\nBits per second: {}".format(time.time() - curTime, bits_read, bits_read /(time.time() - curTime) ))
+			ser.write('4'.encode('utf-8'))
+			# Timer
+			curTime = time.time()
+			bytes_read = self.getData()
+			self.updateView()
+			print("Time: {}\nBytes read: {}\nBytes per second: {}".format(time.time() - curTime, bytes_read, bytes_read /(time.time() - curTime) ))
 		except serial.serialutil.SerialException:
 			lb.insert(END, 'Could not run test 4')
 			self.updateView()
@@ -161,47 +162,71 @@ class Motor(Frame):
 		# Perform handshake with TIVA
 		while True:
 			try:
-				send = hs_bit
+				send = hs_byte
 				# Wait until there is something I can read
 				#while ser.inWaiting() == 0:
-				for i in range(0,51)
+				for i in range(0,51):
 					ser.write(send)
-					# print('Wrote handshake')
 					time.sleep(0.1) # sleep for 100 ms
+
+					if ser.inWaiting() >= 1:
+						break
+
 					if i == 50:
-						lb.insert(END, 'No handshake bit received...')
+						lb.insert(END, 'No handshake byte received...')
+						self.updateView()
 						return
 					elif i % 10 == 0:
-						
+						lb.insert(END, '.')
+						self.updateView()
 					continue
-				bit = ser.read()
-				lb.insert(END, 'Received text: {}'.format(bit))
-				if (bit == send):
+
+				byte = ser.read()
+				lb.insert(END, 'Received text: {}'.format(byte))
+				self.updateView()
+				if (byte == send):
 					lb.insert(END, 'Handshake complete')
+					self.updateView()
 					break
 			except serial.SerialTimeoutException:
 				lb.insert(END, 'Nothing received...')
+				self.updateView()
 
 	# Read in and print data
 	def getData(self):
-		total_bits = 0
+
+		filetime = time.localtime()
+		f = open('test_{}_{}_{}_{}{}{}.csv'.format(filetime[1], filetime[2], filetime[0], filetime[3], filetime[4], filetime[5]), 'w')
+
+		line1 = 'Time, Input, RPM, Voltage, Current, Thrust, Torque\n'
+		lb.insert(END, line1)
+		self.updateView()
+		f.write(line1)
+
+
+		total_bytes = 0
 		buf = [] # use as buffer
 		listLine = ''
-		bit = ser.read()
-		total_bits += 1 # Debug
+		byte = ser.read() # Read in first byte
+		total_bytes += 1 # Debug
+		ender = 0
+		index = 0
 
-		# Flush buffer of handshake bits
-		while bit == hs_bit:
-			bit = ser.read()
-			total_bits += 1 # Debug
+		# Flush buffer of handshake bytes
+		while byte == hs_byte:
+			byte = ser.read()
+			total_bytes += 1 # Debug
 
 		while True:
-			# Check to see if read bit is comma or newline
-			if bit != b',' and bit != b'\n' and bit != b'!':
-				buf.append(bit)
+			# Check if byte is an ender byte
+			if byte == termination_byte:
+				ender += 1
+				if ender == 4:
+					break
+			else: ender = 0
 
-			# If bit is comma or newline, convert hex data to dec and reset data
-			elif bit == b',':
+			if index  == 3 or index == 4 or index == 8 or index == 10 or index == 12 or index == 14 :
+				buf.append(byte)
 				try:
 					# Convert bytes to usable int
 					data = int.from_bytes(b''.join(buf), byteorder = 'big')
@@ -211,16 +236,15 @@ class Motor(Frame):
 					'''
 					listLine += str(data)
 					listLine += ', '
-					# lb.insert(END, data)
-					# self.updateView()
 
 					buf = [] # Clear buffer
+					index += 1
 
 				except ValueError:
-					print(bit)
+					print(byte)
 
-			# If bit is newline, do the same as comma except go to next line in file
-			elif bit == b'\n':
+			elif index == 16:
+				buf.append(byte)
 				try:
 					# Convert bytes to usable int
 					data = int.from_bytes(b''.join(buf), byteorder = 'big')
@@ -231,22 +255,78 @@ class Motor(Frame):
 					listLine += str(data)
 					lb.insert(END, listLine)
 					self.updateView()
+					f.write(listLine)
+					f.write('\n')
 
 					listLine = '' # Clear listLine
 					buf = [] # Clear buffer
+
+					index = 0
+
 				except ValueError:
-					print(bit)
+					print(byte)
 
-			elif bit == b'!':
-				break
+			else:
+				buf.append(byte)
+				index += 1
+
+			# Read next byte
+			byte = ser.read()
+			# print(byte)
+			total_bytes += 1
 
 
-			# Read next bit
-			bit = ser.read()
-			# print(bit)
-			total_bits += 1
+			# # Check to see if read byte is comma or newline
+			# if byte != b',' and byte != b'\n' and byte != b'!':
+			# 	buf.append(byte)
 
-		return total_bits
+			# # If byte is comma or newline, convert hex data to dec and reset data
+			# elif byte == b',':
+			# 	try:
+			# 		# Convert bytes to usable int
+			# 		data = int.from_bytes(b''.join(buf), byteorder = 'big')
+
+			# 		'''
+			# 		Debug Statement
+			# 		'''
+			# 		listLine += str(data)
+			# 		listLine += ', '
+			# 		# lb.insert(END, data)
+			# 		# self.updateView()
+
+			# 		buf = [] # Clear buffer
+
+			# 	except ValueError:
+			# 		print(byte)
+
+			# # If byte is newline, do the same as comma except go to next line in file
+			# elif byte == b'\n':
+			# 	try:
+			# 		# Convert bytes to usable int
+			# 		data = int.from_bytes(b''.join(buf), byteorder = 'big')
+
+			# 		'''
+			# 		Debug Statement
+			# 		'''
+			# 		listLine += str(data)
+			# 		lb.insert(END, listLine)
+			# 		self.updateView()
+
+			# 		listLine = '' # Clear listLine
+			# 		buf = [] # Clear buffer
+			# 	except ValueError:
+			# 		print(byte)
+
+			# elif byte == b'!':
+			# 	break
+
+
+			# # Read next byte
+			# byte = ser.read()
+			# print(byte)
+			# total_bytes += 1
+		f.close()
+		return total_bytes
 
 
 	# Update the listbox 
