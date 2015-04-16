3b3121d7a25a3d4afb16ed1679adeed92413460f
Andrew Williams <williamsr.andrew@gmail.com>
2015-04-16 18:14:06 -0500

    Fixed encoding issue
    Fixed encoding issue

--- Python Test Code/threadsTest.py
+++ Python Test Code/threadsTest.py
@@ -4,13 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-global dataset
-global end_run
-dataset = []
-end_run = False
-thread_sleep = 0.1
 
-def drawData():
+
+def drawData(dataQ):
 		plt.xlabel('Time (s)')
 		plt.ylabel('Data')
 		plt.ion()
@@ -30,32 +26,40 @@ def drawData():
 				plt.plot(t, data, 'b:')
 				plt.draw()
 				didGet = False
-			time.sleep(thread_sleep)
+			time.sleep(1)
 			if dataSet == False:
 				break
 		plt.show(block = True)
 		return
 
-cur_time = time.time()
-dataset.append([])
-dataset.append([])
-# dataset.append([])
-index = 0
-dataQ = mp.Queue()
-draw_job = mp.Process(target = drawData, args = ())
-draw_job.start()
-
-while time.time() - cur_time < 10:
-	dataQ.put([time.time() - cur_time, index])
-	index += 1
-	time.sleep(0.0001)
-
-dataQ.put(False)
-# print (dataset)
-draw_job.join()
 
 
+def main():
+	global dataset
+	global end_run
+	dataset = []
+	end_run = False
+	thread_sleep = 0.1
+	cur_time = time.time()
+	dataset.append([])
+	dataset.append([])
+	# dataset.append([])
+	index = 0
+	dataQ = mp.Queue()
+	draw_job = mp.Process(target = drawData, args = (dataQ,))
+	draw_job.start()
 
+	while time.time() - cur_time < 10:
+		dataQ.put([time.time() - cur_time, index])
+		index += 1
+		time.sleep(0.001)
+
+	dataQ.put(False)
+	# print (dataset)
+	draw_job.join()
+
+if __name__ == '__main__':
+	main()
 
 
 
