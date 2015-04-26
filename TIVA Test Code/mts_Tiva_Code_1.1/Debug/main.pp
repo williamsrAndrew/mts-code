8835e372535c556b514d42172f44970861f5b15e
timothymcdnld <timothymcdnld@gmail.com>
2015-04-25 19:34:07 -0500

    New Version of TIVA Code

---
+++ TIVA Test Code/mts_Tiva_Code_1.1/Debug/main.pp
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_ints.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_i2c.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pwm.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/i2c.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/timer.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h
+
+../main.c: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h: 
